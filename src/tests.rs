bf7ad93a87f1ebfb2307a2e33e4c6ec0ff9e9621
kiwidoggie <forums@allenthinks.com>
2021-08-08 05:22:10 -0700

    Experiment with process starting

--- src/tests.rs
+++ src/tests.rs
@@ -7,6 +7,8 @@ mod lobby;
 #[path = "server.rs"]
 mod server;
 
+use std::{io::{BufRead, BufReader}, process::{Command, Stdio}};
+
 #[test]
 fn load_banlist_test() {
     let mut deploy = vu::VuDeploy::new();
@@ -22,4 +24,26 @@ fn lobby_expiration_test() {
 #[test]
 fn experimental_process_test() {
     let is_win32 = cfg!(target_os = "windows");
+
+    // C:\Users\godiwik\AppData\Local\VeniceUnleashed\client\vu.exe -server -dedicated -high60
+    let child_process_spawn = Command::new("").stdout(Stdio::piped()).spawn();
+    
+    match child_process_spawn {
+        Ok(mut child_process) => {
+            let rip = child_process.stdout.as_mut().unwrap();
+            let mut reader = BufReader::new(rip);
+            let mut line: String = String::new();
+            for i in 0..10 {
+                reader.read_line(&mut line);
+
+                println!("line: ({}).", line);
+            }
+        },
+        Err(err) => {
+            eprintln!("err: ({}).", err);
+            assert!(false);
+        }
+    }
+
+    
 }
